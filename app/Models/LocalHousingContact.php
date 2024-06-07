20bb41ac68708d07eb504003b12374dc2ac62ec2
Iverson <ivcotad@gmail.com>
2024-06-07 09:30:07 -0700

    add local housing contact model

---
+++ app/Models/LocalHousingContact.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class LocalHousingContact extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'congregation',
+    ];
+
+
+    function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -46,4 +46,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function localHousingContact()
+    {
+        return $this->hasOne(LocalHousingContact::class);
+    }
 }
