1a729cc77195cdc42ec122eb4cdbf476806c67ed
Iverson <ivcotad@gmail.com>
2024-06-08 06:04:05 -0700

    add react hot toast dep

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -1,24 +1,41 @@
-import './bootstrap';
-import '../css/app.css';
+import "./bootstrap";
+import "../css/app.css";
 
-import { createRoot, hydrateRoot } from 'react-dom/client';
-import { createInertiaApp } from '@inertiajs/react';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { createRoot, hydrateRoot } from "react-dom/client";
 
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+import { Toaster } from "react-hot-toast";
+import { createInertiaApp } from "@inertiajs/react";
+import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
+
+const appName = import.meta.env.VITE_APP_NAME || "Laravel";
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
-    resolve: (name) => resolvePageComponent(`./Pages/${name}.tsx`, import.meta.glob('./Pages/**/*.tsx')),
+    resolve: (name) =>
+        resolvePageComponent(
+            `./Pages/${name}.tsx`,
+            import.meta.glob("./Pages/**/*.tsx")
+        ),
     setup({ el, App, props }) {
         if (import.meta.env.DEV) {
-            createRoot(el).render(<App {...props} />);
-            return
+            createRoot(el).render(
+                <>
+                    <App {...props} />
+                    <Toaster />
+                </>
+            );
+            return;
         }
 
-        hydrateRoot(el, <App {...props} />);
+        hydrateRoot(
+            el,
+            <>
+                <App {...props} />
+                <Toaster />
+            </>
+        );
     },
     progress: {
-        color: '#4B5563',
+        color: "#4B5563",
     },
 });
