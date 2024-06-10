d39abe889b0a0733de4ab7b39f1014c4d0edca04
Iverson <ivcotad@gmail.com>
2024-06-10 08:13:52 -0700

    add sqlite testing

--- config/database.php
+++ config/database.php
@@ -30,7 +30,11 @@ return [
     */
 
     'connections' => [
-
+        'testing' => [
+            'driver' => 'sqlite',
+            'database' => ':memory:',
+            'prefix' => '',
+        ],
         'sqlite' => [
             'driver' => 'sqlite',
             'url' => env('DB_URL'),
@@ -144,7 +148,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_') . '_database_'),
         ],
 
         'default' => [
