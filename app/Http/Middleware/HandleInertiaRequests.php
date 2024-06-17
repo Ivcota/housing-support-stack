6281a1708501c2a5a3acd5181e0c373817e2a1fe
Iverson <ivcotad@gmail.com>
2024-06-16 20:15:53 -0700

    fixup! basic auth inertia middleware

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -39,7 +39,7 @@ class HandleInertiaRequests extends Middleware
                 ...(new Ziggy)->toArray(),
                 'location' => $request->url(),
             ],
-            'canViewAdmin' => $request->user()->can('view-admin'),
+            'canViewAdmin' => $request->user()?->can('view-admin'),
         ];
     }
 }
