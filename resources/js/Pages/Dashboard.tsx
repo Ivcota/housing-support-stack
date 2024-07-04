4dd1fa265723ac333cfa74ba4ba93aba5138cc94
Iverson <ivcotad@gmail.com>
2024-07-04 07:13:57 -0700

    add dashboard title

--- resources/js/Pages/Dashboard.tsx
+++ resources/js/Pages/Dashboard.tsx
@@ -1,4 +1,4 @@
-import { router, useForm } from "@inertiajs/react";
+import { Head, router, useForm } from "@inertiajs/react";
 
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import { Button } from "@/Components/ui/button";
@@ -30,60 +30,63 @@ export default function Dashboard({ auth }: PageProps) {
     });
 
     return (
-        <AuthenticatedLayout user={auth.user}>
-            <div className="bg-white max-w-7xl mx-auto">
-                <div className="px-9 bg-white py-6 flex flex-col gap-3">
-                    <h1 className="text-xl font-bold">
-                        Hey {auth.user.name} 👋
-                    </h1>
-                    <p>
-                        Welcome to your survey dashboard. Submit surveys and
-                        view their progress.
-                    </p>
-                    <Button
-                        className="max-w-fit"
-                        onClick={() => router.get("/upload-survey")}
+        <>
+            <Head title="Dashboard" />
+            <AuthenticatedLayout user={auth.user}>
+                <div className="bg-white max-w-7xl mx-auto">
+                    <div className="px-9 bg-white py-6 flex flex-col gap-3">
+                        <h1 className="text-xl font-bold">
+                            Hey {auth.user.name} 👋
+                        </h1>
+                        <p>
+                            Welcome to your survey dashboard. Submit surveys and
+                            view their progress.
+                        </p>
+                        <Button
+                            className="max-w-fit"
+                            onClick={() => router.get("/upload-survey")}
+                        >
+                            Upload a Survey
+                        </Button>
+                    </div>
+                    <div
+                        className="flex items-end gap-2 justify-between px-9 max-w-80"
+                        role="list"
                     >
-                        Upload a Survey
-                    </Button>
+                        <form
+                            role="group"
+                            className="flex flex-col"
+                            onSubmit={(e) => {
+                                e.preventDefault();
+                                get("/dashboard");
+                            }}
+                        >
+                            <label htmlFor="search" className="text-gray-700">
+                                Search
+                            </label>
+                            <Input
+                                type="search"
+                                placeholder="Search something..."
+                                onChange={(e) =>
+                                    setData({
+                                        search: e.target.value,
+                                    })
+                                }
+                            />
+                        </form>
+                        <Button
+                            onClick={(e) => {
+                                e.preventDefault();
+                                router.get("/dashboard");
+                            }}
+                            variant="outline"
+                        >
+                            Clear
+                        </Button>
+                    </div>
+                    <Surveys />
                 </div>
-                <div
-                    className="flex items-end gap-2 justify-between px-9 max-w-80"
-                    role="list"
-                >
-                    <form
-                        role="group"
-                        className="flex flex-col"
-                        onSubmit={(e) => {
-                            e.preventDefault();
-                            get("/dashboard");
-                        }}
-                    >
-                        <label htmlFor="search" className="text-gray-700">
-                            Search
-                        </label>
-                        <Input
-                            type="search"
-                            placeholder="Search something..."
-                            onChange={(e) =>
-                                setData({
-                                    search: e.target.value,
-                                })
-                            }
-                        />
-                    </form>
-                    <Button
-                        onClick={(e) => {
-                            e.preventDefault();
-                            router.get("/dashboard");
-                        }}
-                        variant="outline"
-                    >
-                        Clear
-                    </Button>
-                </div>
-                <Surveys />
-            </div>
-        </AuthenticatedLayout>
+            </AuthenticatedLayout>
+        </>
     );
 }
